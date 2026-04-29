72c09bd2c3ae2bafff15de5b169c6dd76a9d5a02
haujul11 <11haujul@stud.akademiet.no>
2026-04-29 22:43:00 +0200

    Setter opp Flask-applikasjon og startpunkt

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask
-
+from .routes import bp
 
 def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "enkel-hemmelig-nok-for-skole"
+    base_dir = os.path.dirname(__file__)
+    templates_dir = os.path.join(base_dir, "templates")
+    static_dir = os.path.join(base_dir, "static")
 
-    # Kobler på routes (Blueprint)
-    from .routes import bp
+    app = Flask(__name__, template_folder=templates_dir, static_folder=static_dir)
     app.register_blueprint(bp)
-
+    app.secret_key = "hemmelig-nok"
+    print("TEMPLATES:", app.jinja_loader.searchpath)
+    print("STATIC:", app.static_folder)
     return app
